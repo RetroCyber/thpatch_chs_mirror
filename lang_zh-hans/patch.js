8b4ed51803fc74b5dcb3a096fe7c526a452e301e
ROCO2017 <1401548627@qq.com>
2021-12-06 22:16:51 +0800

    中文补丁文件上传。

---
+++ lang_zh-hans/patch.js
@@ -0,0 +1,12 @@
+{
+    "dependencies": [
+        "nmlgc/base_tsa",
+        "nmlgc/base_tasofro"
+    ],
+    "id": "lang_zh-hans",
+    "servers": [
+        "https://srv.thpatch.net/lang_zh-hans/"
+    ],
+    "title": "Simplified Chinese language pack",
+    "update": true
+}
---
+++ lang_zh-hans/th155/nut_strings.js
@@ -0,0 +1,19 @@
+{
+    "Yukari boss 2": "黑色子弹对下仆无效",
+    "New version 1": "发现新版本",
+    "New version 2": "重启以应用更新",
+    "Random Select": "随机选择",
+    "Game title": "东方凭依华　～ Antinomy of Common Flowers.",
+    "No change": "不指定",
+    "Joon 1": "A:近打击　B:射击　C:符卡射击",
+    "Dir creation failed": "执行程序所需的目录创建失败了。",
+    "Yukari boss": "切换到下仆来躲避红色蓄力攻击！",
+    "Save replay": "保存Replay？",
+    "Continue": "要Continue（续关）吗？",
+    "Yes": "是的",
+    "No": "算了",
+    "Delete file": "删除这个文件吗？",
+    "No file": "这里什么也没有",
+    "Loading": "读取中……",
+    "Can't use": "无法打开此文件。"
+}
